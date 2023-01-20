8da342d5b451c795c77eddb830816c69cbca3321
OmarElati <5796@holbertonstudents.com>
2023-01-20 23:29:58 +0100

    Print last digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number: int) -> int:
-    last_digit = abs(number) % 10
+    last_digit = str(abs(number) % 10)
     print(last_digit)
+    print(f"({len(last_digit)} chars long)")
