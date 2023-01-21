d95c1f49d20505ebf0dcdf1cdc9d3bf2a9af7228
OmarElati <5796@holbertonstudents.com>
2023-01-21 18:11:23 +0100

    imports functions from the file calculator_1.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
 result_add = add(a, b)
 result_sub = sub(a, b)
 result_mul = mul(a, b)
