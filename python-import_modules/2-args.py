daec5be60cddd25172595c138d8a9eb02357b836
OmarElati <5796@holbertonstudents.com>
2023-01-23 13:51:56 +0100

    prints the number of and the list of its arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    num_args = len(argv)
+    if num_args == 1:
+        print(f"{num_args} argument:")
+    else:
+        print(f"{num_args} arguments:")
+    for i, arg in enumerate(argv, start=1):
+        print(f"{i}: {arg}")
+    if num_args == 0:
+        print(".")
+    else:
+        print(":")
