6d68f400d4c9d5c8e92362e66990de0a7c61e49d
OmarElati <5796@holbertonstudents.com>
2023-01-27 20:46:15 +0100

    Print a list of integers

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in my_list:
+def print_list_integer(my_list=[], start=0, end=-1):
+    if end == -1:
+        end = len(my_list)
+    for i in my_list[start:end]:
         print("{}".format(i))
