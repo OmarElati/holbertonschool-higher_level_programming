472f34c0066a88f8d4ac89fa23feccf935058543
OmarElati <5796@holbertonstudents.com>
2023-01-27 21:55:40 +0100

    prints a matrix of integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=' ')
+        for i,val in enumerate(row):
+            print("{:d}".format(val), end=" ")
         print()
