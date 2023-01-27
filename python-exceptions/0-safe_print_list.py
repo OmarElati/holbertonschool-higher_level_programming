8e7216e026a04657e5ca42b3a5b6cf3a09499d6a
OmarElati <5796@holbertonstudents.com>
2023-01-27 23:08:09 +0100

    prints x elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    printed_elements = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end=' ')
+            printed_elements += 1
+    except:
+        pass
+    print()
+    return (printed_elements)
