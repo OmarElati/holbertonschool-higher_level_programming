54f8071fd43ec786a470960452fcb0e5b323d0e5
OmarElati <5796@holbertonstudents.com>
2023-01-27 23:17:32 +0100

    prints an integer with {:d}.format()

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(int(value)))
-        return True
-    except:
+    except(ValueError, TypeError):
         return False
+    return True
