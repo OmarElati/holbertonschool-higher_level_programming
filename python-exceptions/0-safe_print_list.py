3f789cf134ad242f7e7a06967cc834c388aa0af8
OmarElati <5796@holbertonstudents.com>
2023-01-27 23:10:50 +0100

    prints x elements of a list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    printed_elements = 0
+    count = 0
     try:
         for i in range(x):
-            print(my_list[i], end=' ')
-            printed_elements += 1
+            print(my_list[i], end="")
+            count += 1
     except:
         pass
     print()
-    return (printed_elements)
+    return count
