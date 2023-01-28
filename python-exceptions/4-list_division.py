a033d1c665b8dbaf9ef1101e183a0a00167bb77d
OmarElati <5796@holbertonstudents.com>
2023-01-28 14:26:07 +0100

    divides element by element 2 lists

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
+result_list = []
+
+
 def list_division(my_list_1, my_list_2, list_length):
-
-	result_list = []
-
-	for i in range(list_length):
-		try:
-			result = my_list_1[i] / my_list_2[i]
-			if not isinstance(my_list_1[i], (int, float)) or not isinstance(my_list_2[i], (int, float)):
-				raise TypeError("wrong type")
-		except TypeError:
-			print("wrong type")
-			result = 0
-		except ZeroDivisionError:
-			print("division by 0")
-			result = 0
-		except IndexError:
-			print("out of range")
-			result = 0
-		finally:
-			result_list.append(result)
-	return result_list
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+            x = my_list_1[i]
+            y = my_list_2[i]
+            a = isinstance(x, (int, float))
+            b = isinstance(y, (int, float))
+            if not a or not b:
+                raise TypeError("wrong type")
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            result_list.append(result)
+    return result_list
