4405757197744c06cff1288d3872ebdb3df1e2c9
OmarElati <5796@holbertonstudents.com>
2023-01-31 17:51:39 +0100

    class Square that defines a square by: (based on 3-square.py)

---
+++ python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Defines a square by:
+        - size: private instance attribute
+            - Property getter to retrieve it
+            - Property setter to set it
+              (size must be an integer and >= 0)
+        - Instantiation with optional size
+        - Method to return the current square area
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    def size(self):
+        """Retrieve the size attribute"""
+        return self._size
+
+    def size(self, value):
+        """
+        Set the size attribute
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self._size = value
+
+    def area(self):
+        """Return the current square area"""
+        return self.size ** 2
