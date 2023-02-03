836193c45a7995c313324031d5562baea4d43df8
OmarElati <5796@holbertonstudents.com>
2023-02-03 23:48:45 +0100

    class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
 ..
-""" 
+"""
 
 
 class Rectangle:
     """
     ..
     """
-    def __init__(self, width = 0,height = 0):
+    def __init__(self, width=0, height=0):
         """.."""
         self.height = height
         self.width = width
