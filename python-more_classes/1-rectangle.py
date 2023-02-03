e1ee9a61ee964082a611af62f2d013f68107c2e5
OmarElati <5796@holbertonstudents.com>
2023-02-03 21:36:43 +0100

    test

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
+"""
+..
+"""
+
+
 class Rectangle:
+    """
+    ..
+    """
     def __init__(self, width=0, height=0):
+        """.."""
         self.height = height
         self.width = width
 
     @property
     def width(self):
+        """.."""
         return self.width
     @property
     def height(self):
+        """.."""
         return self.height
 
     @width.setter
     def set_width(self, width):
+        """.."""
         if width is int :
             if width < 0:
                 raise ValueError('width must be >= 0')
@@ -23,6 +35,7 @@ class Rectangle:
 
     @height.setter
     def set_height(self, height):
+        """.."""
         if height is int :
             if height < 0:
                 raise ValueError('height must be >= 0')
