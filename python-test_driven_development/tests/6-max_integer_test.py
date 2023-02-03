66fe3843fef8bd998f45325fd5b4d0fd4fca5c17
OmarElati <5796@holbertonstudents.com>
2023-02-03 19:49:33 +0100

    Since the beginning you have been creating “Interactive tests”

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -3,38 +3,32 @@
     Since the beginning you have been creating “Interactive tests”.
     For this exercise, you will add Unittests.
 """
+
+
 import unittest
 
-def max_integer(list=[]):
-    """
-    Finds the max integer in a list of integers
-    """
-    if len(list) == 0:
-        return None
-    max_val = list[0]
-    for i in list:
-        if i > max_val:
-            max_val = i
-    return max_val
+max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    def test_empty_list(self):
-        self.assertEqual(max_integer([]), None)
+    """
+        class for cases
+    """
 
-    def test_list_of_integers(self):
-        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
-        self.assertEqual(max_integer([5, 4, 3, 2, 1]), 5)
-
-    def test_list_of_negative_integers(self):
-        self.assertEqual(max_integer([-1, -2, -3, -4, -5]), -1)
-        self.assertEqual(max_integer([-5, -4, -3, -2, -1]), -1)
-
-    def test_list_of_mixed_integers(self):
-        self.assertEqual(max_integer([1, -2, 3, -4, 5]), 5)
-        self.assertEqual(max_integer([-5, 4, -3, 2, -1]), 4)
-
-    def test_max_at_end(self):
-        self.assertEqual(max_integer([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 10)
+    def test_cases(self):
+        """
+            test cases for function
+        """
+        self.assertEqual(max_integer([13, 0, 56, 3]), 56)
+        self.assertEqual(max_integer([-3, -6, -1, -67]), -1)
+        self.assertEqual(max_integer(), None)
+        self.assertEqual(max_integer([0, 0, 0]), 0)
+        self.assertEqual(max_integer([-5, 0, 6]), 6)
+        self.assertEqual(max_integer([73210584, 58, -68, 3]), 73210584)
+        self.assertEqual(max_integer([1, 6]), 6)
+        self.assertEqual(max_integer([87]), 87)
+        self.assertEqual(max_integer([6, 8, 10e1000]), float('inf'))
+        self.assertEqual(max_integer([87, float('nan'), 5]), 87)
+        self.assertEqual(max_integer("Holberton"), 't')
 
 if __name__ == '__main__':
     unittest.main()
