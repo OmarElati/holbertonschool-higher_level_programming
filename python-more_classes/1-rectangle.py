757333eb3f8a0209bb36b62be41950852bb239ca
OmarElati <5796@holbertonstudents.com>
2023-02-03 21:26:48 +0100

    test

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 class Rectangle:
-    def __init__(self, width=0, height=0):
-        self.height = height
-        self.width = width
+    def __init__(self, _Rectangle__width=0, _Rectangle__height=0):
+        self._Rectangle__height = _Rectangle__height
+        self._Rectangle__width = _Rectangle__width
 
-    def set_width(self, width):
-        if width is int :
-            if width < 0:
+    def set__Rectangle__width(self, _Rectangle__width):
+        if _Rectangle__width is int :
+            if _Rectangle__width < 0:
                 raise ValueError('width must be >= 0')
             else :
-                self.width = width
+                self._Rectangle__width = _Rectangle__width
         else :
             raise TypeError('width must be an integer')
 
-    def set_height(self, height):
-        if height is int :
-            if height < 0:
+    def set__Rectangle__height(self, _Rectangle__height):
+        if _Rectangle__height is int :
+            if _Rectangle__height < 0:
                 raise ValueError('height must be >= 0')
             else :
-                self.height = height
+                self._Rectangle__height = _Rectangle__height
         else :
             raise TypeError('height must be an integer')
 
-    def width(self):
-        return self.width
+    def _Rectangle__width(self):
+        return self._Rectangle__width
 
-    def height(self):
-        return self.height
+    def _Rectangle__height(self):
+        return self._Rectangle__height
