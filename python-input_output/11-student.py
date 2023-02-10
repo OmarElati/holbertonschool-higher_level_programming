0203de8d395398e19caf8477f3c18cade74b9a82
OmarElati <5796@holbertonstudents.com>
2023-02-10 21:57:48 +0100

    class Student that defines a student

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+class Student
+"""
+
+
+class Student:
+    """ defines a student """
+    def __init__(self, first_name, last_name, age):
+        """ Instantiation with first_name, last_name and age """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Return a JSON representation """
+        if attrs is None:
+            return {key: value for key, value in self.__dict__.items()}
+        else:
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+
+    def reload_from_json(self, json):
+        """ Reload from a JSON representation """
+        for key, value in json.items():
+            setattr(self, key, value)
