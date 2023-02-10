7bad0eaa85fd56aa11b67018189e004960b48d0c
OmarElati <5796@holbertonstudents.com>
2023-02-10 20:07:58 +0100

    Defines the BaseGeometry class

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Defines the BaseGeometry class
+    """
+    def area(self):
+        """
+        that raises an Exception with the
+        message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
