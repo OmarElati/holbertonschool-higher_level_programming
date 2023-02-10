b14e13364657aec7f78663e72c7e830f50b03789
OmarElati <5796@holbertonstudents.com>
2023-02-10 21:31:58 +0100

    script that adds all arguments to a Python list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,18 +9,25 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """ save to json file """
+    """ Save an object to a file in JSON format """
     with open(filename, 'w') as file:
-        """ save to json file """
         json.dump(my_obj, file)
 
 def load_from_json_file(filename):
-    """ load from json file """
+    """ Load an object from a file in JSON format """
     try:
         with open(filename, 'r') as file:
             return json.load(file)
     except:
         return []
+
+def add_items(filename, items):
+    """ Add items to a list and save the list to a file in JSON format """
+    my_list = load_from_json_file(filename)
+    for item in items:
+        my_list.append(item)
+    save_to_json_file(my_list, filename)
+
 
 filename = "add_item.json"
 my_list = load_from_json_file(filename)
