9faaced801fcf48823109f85ae5df57d56e20f6b
OmarElati <5796@holbertonstudents.com>
2023-02-17 17:38:28 +0100

    file Test

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -6,51 +6,82 @@ from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
     def test_init(self):
-        r1 = Rectangle(10, 20, 0, 0, 1)
-        self.assertEqual(r1.width, 10)
-        self.assertEqual(r1.height, 20)
-        self.assertEqual(r1.x, 0)
-        self.assertEqual(r1.y, 0)
-        self.assertEqual(r1.id, 1)
+        r = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+        self.assertEqual(r.x, 3)
+        self.assertEqual(r.y, 4)
+        self.assertEqual(r.id, 5)
 
-        r2 = Rectangle(5, 5)
-        self.assertEqual(r2.width, 5)
-        self.assertEqual(r2.height, 5)
-        self.assertEqual(r2.x, 0)
-        self.assertEqual(r2.y, 0)
-        self.assertIsNotNone(r2.id)
+    def test_width(self):
+        r = Rectangle(1, 2, 3, 4, 5)
+        r.width = 6
+        self.assertEqual(r.width, 6)
 
-    def test_getters_setters(self):
-        r = Rectangle(10, 20, 0, 0, 1)
+        with self.assertRaises(ValueError):
+            r.width = -1
 
-        r.width = 5
-        self.assertEqual(r.width, 5)
+        with self.assertRaises(TypeError):
+            r.width = "not a number"
 
-        r.height = 10
-        self.assertEqual(r.height, 10)
+    def test_height(self):
+        r = Rectangle(1, 2, 3, 4, 5)
+        r.height = 7
+        self.assertEqual(r.height, 7)
 
-        r.x = 1
-        self.assertEqual(r.x, 1)
+        with self.assertRaises(ValueError):
+            r.height = -1
 
-        r.y = 2
-        self.assertEqual(r.y, 2)
+        with self.assertRaises(TypeError):
+            r.height = "not a number"
+
+    def test_x(self):
+        r = Rectangle(1, 2, 3, 4, 5)
+        r.x = 8
+        self.assertEqual(r.x, 8)
+
+        with self.assertRaises(ValueError):
+            r.x = -1
+
+        with self.assertRaises(TypeError):
+            r.x = "not a number"
+
+    def test_y(self):
+        r = Rectangle(1, 2, 3, 4, 5)
+        r.y = 9
+        self.assertEqual(r.y, 9)
+
+        with self.assertRaises(ValueError):
+            r.y = -1
+
+        with self.assertRaises(TypeError):
+            r.y = "not a number"
 
     def test_area(self):
-        r = Rectangle(10, 20)
-        self.assertEqual(r.area(), 200)
+        r = Rectangle(3, 4)
+        self.assertEqual(r.area(), 12)
 
-        r.width = 5
-        r.height = 5
-        self.assertEqual(r.area(), 25)
+    def test_str(self):
+        r = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(str(r), "[Rectangle] (5) 3/4 - 1/2")
 
     def test_display(self):
         r = Rectangle(2, 3)
+        self.assertEqual(r.display(), None)
 
-        expected_output = "##\n" * 3
-        self.assertEqual(r.display(), expected_output)
+    def test_update(self):
+        r = Rectangle(10, 10, 10, 10)
+        r.update(6, 8, 9, 7, 10)
+        self.assertEqual(str(r), "[Rectangle] (6) 7/10 - 8/9")
+        r.update(7, 5, 5, 5, 5)
+        self.assertEqual(str(r), "[Rectangle] (7) 5/5 - 5/5")
 
-        expected_output = "  ##\n  ##\n  ##\n"
-        self.assertEqual(r.display(x=2, y=1), expected_output)
+    def test_to_dictionary(self):
+        r = Rectangle(1, 2, 3, 4, 5)
+        d = r.to_dictionary()
+        self.assertIsInstance(d, dict)
+        self.assertEqual(d, {'id': 5, 'width': 1, 'height': 2, 'x': 3, 'y': 4})
+
 
 if __name__ == '__main__':
     unittest.main()
