3b16cb4cd7e2ed20c985192dbe341c45834772bc
OmarElati <5796@holbertonstudents.com>
2023-02-17 02:18:56 +0100

    Base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+class Base:
+    """ Base class for classes """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initialize the class """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
