4496218b8a6dd57744f79e00adc8c3a857170f6d
OmarElati <5796@holbertonstudents.com>
2023-02-17 17:44:53 +0100

    Add method to load from JSON file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -10,7 +10,10 @@ import json
 
 class Base:
     """ Base class for classes """
+
+
     __nb_objects = 0
+
 
     def __init__(self, id=None):
         """ Initialize the class """
@@ -22,14 +25,51 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """Return the JSON string representation of a list of dictionaries."""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list of the JSON string representation."""
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes already set."""
+        if cls.__name__ == "Rectangle":
+            obj = cls(1, 1)
+        elif cls.__name__ == "Square":
+            obj = cls(1)
+        obj.update(**dictionary)
+        return obj
+
     @classmethod
     def save_to_file(cls, list_objs):
+        """Write the JSON string representation of a list of objects to a file."""
         if list_objs is None:
             list_objs = []
         filename = cls.__name__ + ".json"
         with open(filename, mode="w", encoding="utf-8") as f:
             f.write(cls.to_json_string([o.to_dictionary() for o in list_objs]))
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of instances from a file containing a JSON string representation."""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, mode="r", encoding="utf-8") as f:
+                list_dicts = cls.from_json_string(f.read())
+                return [cls.create(**d) for d in list_dicts]
+        except FileNotFoundError:
+            return []
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Base instance."""
+        dictionary = {}
+        for key, value in self.__dict__.items():
+            dictionary[key.split('__')[-1]] = value
+        return dictionary
