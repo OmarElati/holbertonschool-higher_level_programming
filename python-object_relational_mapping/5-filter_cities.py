05f9d750040044e7c2b79e9c6978d27019cfdc9a
OmarElati <5796@holbertonstudents.com>
2023-03-17 16:44:49 +0100

    Script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa
+    Script that takes in the name of a state as an argument and
+    lists all cities of that state, using the database hbtn_0e_4_usa
 """
 import MySQLdb
 import sys
