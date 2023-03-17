ed7d9ed9f4b2e7616dc269613a860670468ae3f8
OmarElati <5796@holbertonstudents.com>
2023-03-17 20:22:01 +0100

    Defines the City class with a relationship to the State class using SQLAlchemy

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """
-    Defines the City class with a relationship
-    to the State class using SQLAlchemy
+This module defines the City class.
 """
+
+
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from relationship_state import Base, State
 
 
+Base = declarative_base()
+
+
 class City(Base):
-    """Represents a city for a MySQL database using SQLAlchemy"""
-    __tablename__ = 'cities'
-    id = Column(Integer,
-                autoincrement=True,
-                nullable=False,
-                primary_key=True)
-    name = Column(String(128),
-                  nullable=False)
-    state_id = Column(Integer,
-                      ForeignKey('states.id'),
-                      nullable=False)
+    """
+    This class represents a city entity in the database.
+    """
+
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
