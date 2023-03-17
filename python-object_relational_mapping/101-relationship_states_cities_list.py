0274963edbd288ef040f8f61f3b2373a6261f9de
OmarElati <5796@holbertonstudents.com>
2023-03-17 21:00:07 +0100

    script that lists all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,35 +1,22 @@
 #!/usr/bin/python3
-"""
-Lists all State objects, and corresponding City objects,
-contained in the database hbtn_0e_101_usa
-"""
+# Lists all States and corresponding Cities in the database hbtn_0e_101_usa.
+# Usage: ./101-relationship_states_cities_list.py <mysql username> /
+#                                                 <mysql password> /
+#                                                 <database name>
 import sys
-from model_state import Base, State
-from model_city import City
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
 
-
-if __name__ == '__main__':
-    user = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # Creating engine and binding it to metadata
-    engine = create_engine(f'mysql+mysqldb://{user}:{password}@localhost:3306/{db_name}')
-    Base.metadata.create_all(engine)
-
-    # Creating a configured "Session" class
-    Session = sessionmaker(bind=engine)
-
-    # Creating a Session object
-    session = Session()
-
-    # Querying the database to retrieve all State objects and their City objects
-    result = session.query(State).order_by(State.id).all()
-
-    # Printing the results
-    for state in result:
-        print(f"{state.id}: {state.name}")
-        for city in state.cities:
-            print(f"\t{city.id}: {city.name}")
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    
+    with sessionmaker(bind=engine)() as session:
+        states = session.query(State).order_by(State.id).all()
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+            for city in state.cities:
+                print("    {}: {}".format(city.id, city.name))
