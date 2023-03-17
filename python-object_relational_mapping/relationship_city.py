7fed3b5a7e3e7df3fb00cd289a9500d3443186b4
OmarElati <5796@holbertonstudents.com>
2023-03-17 21:40:52 +0100

    This module defines a City model that represents a city for a MySQL database.

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
+
 """
-City class that inherits from Base.
+This module defines a City model that represents a city for a MySQL database. The City model inherits from SQLAlchemy's Base class and is linked to the MySQL table 'cities'.
+
+Attributes:
+    __tablename__ (str): The name of the MySQL table used for this model.
+    id (sqlalchemy.Column): The city's id.
+    name (sqlalchemy.Column): The city's name.
+    state_id (sqlalchemy.Column): The city's state id.
 """
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
+# create a declarative base instance
+Base = declarative_base()
 
 class City(Base):
     """
-    City class that inherits from Base.
+    Represents a city for a MySQL database.
+
+    Attributes:
+        id (sqlalchemy.Column): The city's id.
+        name (sqlalchemy.Column): The city's name.
+        state_id (sqlalchemy.Column): The city's state id.
     """
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    # set the name of the MySQL table used for this model
+    __tablename__ = "cities"
+
+    # define columns for the cities table
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
