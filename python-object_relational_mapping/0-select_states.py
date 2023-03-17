7719f5040955e1bf3ee5c89a9f41eaa17c825563
OmarElati <5796@holbertonstudents.com>
2023-03-17 15:27:51 +0100

    script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+""" script that lists all states from the database hbtn_0e_0_usa """
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
+    """ list all states from the database hbtn_0e_0_usa """
     if len(sys.argv) != 4:
         print("Usage: {} username password database".format(sys.argv[0]))
         sys.exit(1)
