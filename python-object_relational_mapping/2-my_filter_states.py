13d37d1457b4a90420f21df4853773d5e457f596
OmarElati <5796@holbertonstudents.com>
2023-03-17 16:21:44 +0100

    Script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
 """
-    Script that takes in an argument and displays all values in the states
-    table of hbtn_0e_0_usa where name matches the argument
+connect to database
 """
-
-import MySQLdb
-import sys
-
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
+    import MySQLdb
+    from sys import argv
 
     # Open database connection
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=db_name, charset="utf8")
-
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+    )
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
-
-    # execute SQL query using execute() method.
-    cursor.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC"
-                   .format(state_name))
-
-    # Fetch a single row using fetchone() method.
-    rows = cursor.fetchall()
-
-    for row in rows:
-        print(row)
-
-    # disconnect from server
+    # Drop table if it already exist using execute() method.
+    cursor.execute("SELECT * FROM states WHERE BINARY name = '{:s}' \
+    ORDER BY id ASC".format(argv[4]))
+    data = cursor.fetchone()
+    while (data):
+        print(data)
+        data = cursor.fetchone()
     cursor.close()
     db.close()
