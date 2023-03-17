46919a363526a1621c150496e5dc89320faa76bc
OmarElati <5796@holbertonstudents.com>
2023-03-17 20:07:21 +0100

    Defines the City class with a relationship to the State class using SQLAlchemy

---
+++ python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines the City class with a relationship to the State class
+    using SQLAlchemy"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base, State
+
+
+class City(Base):
+    """Represents a city for a MySQL database using SQLAlchemy"""
+    __tablename__ = 'cities'
+    id = Column(Integer,
+                autoincrement=True,
+                nullable=False,
+                primary_key=True)
+    name = Column(String(128),
+                  nullable=False)
+    state_id = Column(Integer,
+                      ForeignKey('states.id'),
+                      nullable=False)
