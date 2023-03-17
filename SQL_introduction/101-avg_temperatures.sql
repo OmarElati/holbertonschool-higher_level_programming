0bb44b27f69e6605e0bbd0f52d07e15ad458c9ba
OmarElati <5796@holbertonstudents.com>
2023-03-17 19:06:11 +0100

    Import in hbtn_0c_0 database this table dump.

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,5 @@
 -- Import in hbtn_0c_0 database this table dump.
-USE hbtn_0c_0;
-SELECT city, AVG((temperature - 32) * 5/9) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
