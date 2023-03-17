1a9903a7837356919ada18cc6536afd0c767ead4
OmarElati <5796@holbertonstudents.com>
2023-03-17 11:56:09 +0100

    script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
-CREATE USER 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
