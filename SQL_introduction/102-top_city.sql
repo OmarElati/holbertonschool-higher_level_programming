b5721bc4199918d10851b9d128598ca489e87cd0
OmarElati <5796@holbertonstudents.com>
2023-03-17 19:13:02 +0100

    Import in hbtn_0c_0 database this table dump.

--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Import in hbtn_0c_0 database this table dump.
 SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
-WHERE month IN ('July', 'August')
+WHERE `month` = 7 OR 'month' = 8
 GROUP BY `city`
 ORDER BY `avg_temp` DESC
 LIMIT 3;
