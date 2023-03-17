de864a21497cc341419cac9fca7e5039400ef1fb
OmarElati <5796@holbertonstudents.com>
2023-03-17 21:41:31 +0100

    State class that defines a State model.

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-State class that inherits from Base.
+State class that defines a State model.
+Inherits from SQLAlchemy Base and links to the MySQL table states.
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
@@ -10,7 +11,13 @@ from model_state import Base
 
 class State(Base):
     """
-    State class that inherits from Base.
+    Represents a state for a MySQL database.
+
+    Attributes:
+        id (sqlalchemy.Column): The state's id.
+        name (sqlalchemy.Column): The state's name.
+        cities (sqlalchemy.orm.relationship): The state's associated cities.
+
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
