979c85bf5469fd7e6c05dece35717895d3a07632
OmarElati <5796@holbertonstudents.com>
2023-03-17 19:32:02 +0100

    Import the database dump from hbtn_0d_tvshows to your MySQL server.

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -4,10 +4,10 @@ FROM `tv_genres` AS `g`
 INNER JOIN `tv_show_genres` AS `sg` ON `sg`.`genre_id` = `g`.`id`
 INNER JOIN `tv_shows` AS `s` ON `s`.`id` = `sg`.`show_id`
 WHERE `g`.`name` NOT IN (
-  SELECT `g`.`name`
-  FROM `tv_genres` AS `g`
-  INNER JOIN `tv_show_genres` AS `sg` ON `sg`.`genre_id` = `g`.`id`
-  INNER JOIN `tv_shows` AS `s` ON `s`.`id` = `sg`.`show_id`
-  WHERE `s`.`title` = 'Dexter'
+    SELECT `g`.`name`
+    FROM `tv_genres` AS `g`
+    INNER JOIN `tv_show_genres` AS `sg` ON `sg`.`genre_id` = `g`.`id`
+    INNER JOIN `tv_shows` AS `s` ON `s`.`id` = `sg`.`show_id`
+    WHERE `s`.`title` = 'Dexter'
 )
 ORDER BY `g`.`name`;
