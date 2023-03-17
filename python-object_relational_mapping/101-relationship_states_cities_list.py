b50376ec21d3a71a4bfe1487be053fc0d7b932fb
OmarElati <5796@holbertonstudents.com>
2023-03-17 21:02:24 +0100

    script that lists all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    
+
     with sessionmaker(bind=engine)() as session:
         states = session.query(State).order_by(State.id).all()
         for state in states:
