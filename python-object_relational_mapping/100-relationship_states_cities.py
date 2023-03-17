c9728b6dadf797d02d433f2295cf3e2af08f2e91
OmarElati <5796@holbertonstudents.com>
2023-03-17 22:22:18 +0100

    test

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,22 +10,22 @@ from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
 
-
 if __name__ == "__main__":
     engine = create_engine(
-                            'mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(
-                                        sys.argv[1],
-                                        sys.argv[2],
-                                        sys.argv[3]
-                                            ),
-                            pool_pre_ping=True
-                                )
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     new_State = State(name="California")
     new_State.cities = [City(name="San Francisco")]
-    re = session.add(new_State)
+    session.add(new_State)
     session.commit()
+
+    # print all the states and their cities
+    states = session.query(State).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
     session.close()
--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -3,14 +3,18 @@
 
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
-
 
 class State(Base):
     """state class"""
     __tablename__ = 'states'
     id = Column(Integer, unique=True, primary_key=True)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
+
+    def __init__(self, name):
+        self.name = name
+        self.cities = []
