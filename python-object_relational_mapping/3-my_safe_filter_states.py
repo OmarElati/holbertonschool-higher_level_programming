a37fbef072518ef179db70978e8dfebb81b989a4
OmarElati <5796@holbertonstudents.com>
2023-03-17 16:29:11 +0100

    Connect to a MySQL server and safely fetch data

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Connect to a MySQL server and safely fetch data
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    # Open database connection
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+    )
+
+    # Prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Execute the query using the execute() method
+    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (argv[4],))
+
+    # Fetch all the rows using fetchall() method
+    rows = cursor.fetchall()
+
+    # Print the data
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
