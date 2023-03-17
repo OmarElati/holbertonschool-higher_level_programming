377f787bdf1cfdbb3a86900486ac2f1cbe410c5c
OmarElati <5796@holbertonstudents.com>
2023-03-17 19:27:57 +0100

    Import the database dump from hbtn_0d_tvshows to your MySQL server.

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,17 +1,13 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server.
-SELECT `tv_genres.name`
-FROM `tv_genres`
-LEFT JOIN `tv_show_genres` ON `tv_genres.id` = `tv_show_genres.genre_id`
-WHERE `tv_show_genres.show_id` = (
-    SELECT `id`
-    FROM `tv_shows`
-    WHERE `title` = 'Dexter'
+SELECT DISTINCT `g`.`name`
+FROM `tv_genres` AS `g`
+INNER JOIN `tv_show_genres` AS `sg` ON `sg`.`genre_id` = `g`.`id`
+INNER JOIN `tv_shows` AS `s` ON `s`.`id` = `sg`.`show_id`
+WHERE `g`.`name` NOT IN (
+  SELECT `g`.`name`
+  FROM `tv_genres` AS `g`
+  INNER JOIN `tv_show_genres` AS `sg` ON `sg`.`genre_id` = `g`.`id`
+  INNER JOIN `tv_shows` AS `s` ON `s`.`id` = `sg`.`show_id`
+  WHERE `s`.`title` = 'Dexter'
 )
-AND `tv_genres.id` NOT IN (
-  SELECT `tv_genres.id`
-  FROM `tv_genres`
-  JOIN `tv_show_genres` ON `tv_genres.id` = `tv_show_genres.genre_id`
-  JOIN `tv_shows` ON `tv_show_genres.show_id` = `tv_shows.id`
-  WHERE `tv_shows.title` = 'Dexter'
-)
-ORDER BY `tv_genres.name` ASC;
+ORDER BY `g`.`name`;
