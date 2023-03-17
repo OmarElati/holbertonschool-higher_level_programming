14f80369ff2eb2a4e9ec8888dd8748c1a5b21101
OmarElati <5796@holbertonstudents.com>
2023-03-17 21:53:07 +0100

    test

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -17,7 +17,6 @@ class State(Base):
         id (sqlalchemy.Column): The state's id.
         name (sqlalchemy.Column): The state's name.
         cities (sqlalchemy.orm.relationship): The state's associated cities.
-
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
