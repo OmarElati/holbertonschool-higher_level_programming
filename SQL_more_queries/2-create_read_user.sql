61a56b514cc1e4101959f00f67b69ae3c418e161
OmarElati <5796@holbertonstudents.com>
2023-03-17 12:11:20 +0100

    script that creates the database hbtn_0d_2 and the user user_0d_2.

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,6 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+DROP USER IF EXISTS 'user_0d_2'@'localhost';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
