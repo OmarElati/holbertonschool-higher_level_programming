4955e64fb47868ff79661038b6c4bf68d3228d14
OmarElati <5796@holbertonstudents.com>
2023-03-17 21:01:28 +0100

    script that lists all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# Lists all States and corresponding Cities in the database hbtn_0e_101_usa.
-# Usage: ./101-relationship_states_cities_list.py <mysql username> /
-#                                                 <mysql password> /
-#                                                 <database name>
+"""
+Lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
