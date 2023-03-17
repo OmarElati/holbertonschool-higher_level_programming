eb86a46190e10998d540e76d1f27b2256ca81576
OmarElati <5796@holbertonstudents.com>
2023-03-17 21:15:38 +0100

    script that creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-# Creates the State “California” with the City “San Francisco”
-# from the database hbtn_0e_100_usa.
-# Usage: ./100-relationship_states_cities.py <mysql username> /
-#                                            <mysql password> /
-#                                            <database name>
+"""
+script that creates the State “California” with the
+City “San Francisco” from the database hbtn_0e_100_usa
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
