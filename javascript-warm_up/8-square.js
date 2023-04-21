d14231d092df190e1a7831b7ac7692bb72efef30
OmarElati <5796@holbertonstudents.com>
2023-04-21 23:33:25 +0100

    javascript is amazing

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@
 const size = parseInt(process.argv[2]);
 
 if (isNaN(size)) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    console.log("X".repeat(size));
+    console.log('X'.repeat(size));
   }
 }
