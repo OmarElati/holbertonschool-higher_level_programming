1c7a85068d625fcdb6e520afe7f6f0548f9e8c44
OmarElati <5796@holbertonstudents.com>
2023-04-21 23:32:27 +0100

    javascript is amazing

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const argsCount = process.argv.length;
 
 if (argsCount === 2) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (argsCount === 3) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
