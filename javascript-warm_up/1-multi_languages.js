73060bd462a632057b64447e6f560d6380d171fe
OmarElati <5796@holbertonstudents.com>
2023-04-21 23:29:05 +0100

    multi languges js

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const firstLine = "C is fun";
-const secondLine = "Python is cool";
-const thirdLine = "JavaScript is amazing";
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
 
 console.log(firstLine);
 console.log(secondLine);
