410ee1d03e2d671569ddfca5a986c983dcf925eb
OmarElati <5796@holbertonstudents.com>
2023-04-21 23:55:32 +0100

    javascript is amazing

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const SquareParent = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
+class Square extends SquareParent {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
   }
 }
 
