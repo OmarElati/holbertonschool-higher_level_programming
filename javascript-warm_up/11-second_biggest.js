a9e729dcd354fde3343afce7ec481a634723bdf3
OmarElati <5796@holbertonstudents.com>
2023-04-21 23:07:30 +0100

    second biggest js

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,20 @@
+const args = process.argv.slice(2);
+const arr = args.map(arg => parseInt(arg));
+
+if (arr.length <= 1) {
+  console.log(0);
+} else {
+  let max = -Infinity;
+  let secondMax = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      secondMax = max;
+      max = arr[i];
+    } else if (arr[i] > secondMax && arr[i] < max) {
+      secondMax = arr[i];
+    }
+  }
+
+  console.log(secondMax);
+}
