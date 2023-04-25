b465fbccefeef1459b27effa349955007f602d7f
OmarElati <5796@holbertonstudents.com>
2023-04-25 20:23:45 +0100

    script that writes a string to a file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const args = process.argv.slice(2);
 
 fs.writeFile(args[0], args[1], 'utf-8', error => {
-    if (error){
-        return console.log(error);
-    }
+  if (error) {
+    return console.log(error);
+  }
 });
