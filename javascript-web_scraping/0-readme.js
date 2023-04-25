083b799288e076a1ada15a28d67f8fbbd1eac3b9
OmarElati <5796@holbertonstudents.com>
2023-04-25 20:19:25 +0100

    script that reads and prints the content of a file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+const args = process.argv.slice(2);
+
+fs.readFile(args[0], 'utf-8', (error, data) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(data);
+    }
+});
+  
