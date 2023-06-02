e9925f9cc3d028ed5d84f00c4795122322e26aae
OmarElati <5796@holbertonstudents.com>
2023-06-02 20:54:36 +0100

    cURL body size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+url=$1
+
 # Send a GET request to the URL and store the response in a variable
-curl -sI $1 | grep "Content-Length" | cut -d " " -f2
+response=$(curl -s -w "%{size_download}" -o /dev/null "$url")
+
+echo "$response"
