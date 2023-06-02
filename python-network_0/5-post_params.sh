b63071e9f3b18393aacc2b8b4b2dce02eadd8e17
OmarElati <5796@holbertonstudents.com>
2023-06-02 21:27:20 +0100

    cURL POST parameters

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response.
-
 url=$1
 email="test@gmail.com"
 subject="I will always be here for PLD"
-
 response=$(curl -sX POST -d "email=$email" -d "subject=$subject" "$url")
-
 echo "POST params:"
 echo "    email: $email"
 echo "    subject: $subject"
