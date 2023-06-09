e13cf98eb2ad04c538bfd2de22d0ddc046fc6c9a
OmarElati <5796@holbertonstudents.com>
2023-06-09 20:57:05 +0100

    Response header

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response. """
+""" Python script that takes in a URL,
+sends a request to the URL and displays
+the value of the X-Request-Id variable
+found in the header of the response. """
 from urllib import request
 from sys import argv
 
