d4afae9f7f42790a073cced2a99a3c324627dc85
OmarElati <5796@holbertonstudents.com>
2023-06-09 21:26:51 +0100

    POST an email

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL and an email address, sends a POST request to the passed URL
-with the email as a parameter, and displays the body of the response"""
+"""Python script that takes in a URL and an email address,
+sends a POST request to the passed URL
+with the email as a parameter,
+and displays the body of the response"""
 
 import requests
 from sys import argv
