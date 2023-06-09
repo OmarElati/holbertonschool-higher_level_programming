ad902d2a1e1e58036050e8a59b33147fd24cbc43
OmarElati <5796@holbertonstudents.com>
2023-06-09 21:25:42 +0100

    POST an email

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -6,11 +6,4 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    url = argv[1]
-    email = argv[2]
-
-    payload = {"email": email}
-    response = requests.post(url, data=payload)
-
-    print("Your email is:", email)
-    print(response.text)
+    print(requests.post(argv[1], data={'email': argv[2]}).text)
