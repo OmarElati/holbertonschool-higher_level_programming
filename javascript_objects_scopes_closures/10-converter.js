e924d9a68756d670d986290ecc6fa45852a7fbdb
OmarElati <5796@holbertonstudents.com>
2023-07-07 18:38:44 +0200

    Number conversion

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
 exports.converter = function (base) {
   return function (number) {
-    if (number < base) {
-      return number.toString();
+    if (number === 0) {
+      return '0';
     } else {
-      return exports.converter(base)(Math.floor(number / base)) + (number % base < 10 ? (number % base).toString() : String.fromCharCode(55 + (number % base)));
+      let result = '';
+      while (number > 0) {
+        const digit = number % base;
+        result = (digit < 10 ? digit : String.fromCharCode(87 + digit)) + result;
+        number = Math.floor(number / base);
+      }
+      return result;
     }
   };
 };
