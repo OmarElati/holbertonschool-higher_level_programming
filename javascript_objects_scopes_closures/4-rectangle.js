e7be711afe043f1819fd04a51adbe6421d6842cc
OmarElati <5796@holbertonstudents.com>
2023-07-07 17:52:16 +0200

    Class Rectangle

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,22 +1,31 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (w, h) {
+  constructor(w, h) {
     if (w <= 0 || h <= 0) {
-      return {}; // Return an empty object
+      return {};
     }
     this.width = w;
     this.height = h;
   }
 
-  print () {
-    let row = '';
+  print() {
+    let row = "";
     for (let i = 0; i < this.width; i++) {
-      row += 'X';
+      row += "X";
     }
     for (let i = 0; i < this.height; i++) {
       console.log(row);
     }
+  }
+
+  rotate() {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
   }
 }
 
